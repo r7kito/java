ea2071be40338a6e6abdb3619ca397f19e481947
r7kito <azizismoilov2701@gmail.com>
2024-10-23 16:23:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+let a =prompt('salom')
+alert('raxmat')
+let b =prompt('ismin nima?')
+alert('mani ismim aziz')
+let c =prompt('nechi yoshdasan')
+alert('man 14 yoshdaman')
+let d =prompt('qayerlik san')
+alert('man Toshkentliman')
+let e =prompt('qayerda yashisan?')
+alert('man qoraqamishdan man')
+ 
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+ 
